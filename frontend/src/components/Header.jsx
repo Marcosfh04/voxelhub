b6047a2513c7417c27b5f25a0151275abb8cd0ee
Juan Camus Robledillo <jcr77@alu.ua.es>
2025-03-27 17:38:06 +0100

    3 VIDEO DONE

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,19 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-// import { useNavigate } from 'react-router-dom'
-// import { useSelector, useDispatch } from 'react-redux'
-// import { logout, reset } from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
 
 function Header() {
-//   const navigate = useNavigate()
-//   const dispatch = useDispatch()
-//   const { user } = useSelector((state) => state.auth)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector((state) => state.auth)
 
-//   const onLogout = () => {
-//     dispatch(logout())
-//     dispatch(reset())
-//     navigate('/')
-//   }
+  const onLogout = () => {
+    dispatch(logout())
+    dispatch(reset())
+    navigate('/')
+  }
 
   return (
     <header className='header'>
@@ -21,14 +21,14 @@ function Header() {
         <Link to='/'>GoalSetter</Link>
       </div>
       <ul>
-        {/* {user ? (
+        {user ? (
           <li>
             <button className='btn' onClick={onLogout}>
               <FaSignOutAlt /> Logout
             </button>
           </li>
         ) : (
-          <> */}
+          <>
             <li>
               <Link to='/login'>
                 <FaSignInAlt /> Login
@@ -39,8 +39,8 @@ function Header() {
                 <FaUser /> Register
               </Link>
             </li>
-          {/* </> */}
-        {/* )} */}
+          </>
+        )}
       </ul>
     </header>
   )
