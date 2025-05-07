3e297187ecc93626edafd0d81d92fbe4a981d56f
Juan Camus Robledillo <jcr77@alu.ua.es>
2025-05-07 22:35:26 +0200

    cosas

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
@@ -9,14 +10,17 @@ const proxyRoutes = require('./rutas/proxyRoutes');
 
 const port = process.env.PORT || 5000
 
+
 connectDB()
 
 const app = express()
 
+
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use('/api/goals', require('./rutas/rutasGoal'))
 app.use('/api/users', require('./rutas/rutasUser'))
 app.use('/api/assets', require('./rutas/rutasAsset'))
 app.use('/api/drive', require('./rutas/rutaDrive'))
